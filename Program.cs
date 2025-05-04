cd3fd48f58e250eb5339f2f340259c4af330dd21
Wim Bervoets <wim.bervoets@pxl.be>
2025-05-04 15:58:51 +0200

    template .net8.0

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var connString = builder.Configuration.GetConnectionString("PeopleConnection");
-builder.Services.AddDbContext<DataContext>(opts =>
+builder.Services.AddDbContext<DataContext>(options =>
 {
-    opts.UseSqlServer(connString);
-    opts.EnableSensitiveDataLogging(true);
+    options.UseSqlServer(connString);
+    options.EnableSensitiveDataLogging(true);
 });
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
@@ -29,13 +29,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.UseEndpoints(endpoints =>
-{
-    endpoints.MapDefaultControllerRoute();
-});
+    {
+        endpoints.MapDefaultControllerRoute();
+    });
 
 SeedData.SeedDatabase(app);
 app.Run();
